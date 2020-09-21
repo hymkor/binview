21bcdb298f76c4f92b8eb0154fc3d3d6eae398d8
HAYAMA_Kaoru <iyahaya@nifty.com>
2020-09-21 10:41:30 +0900

    Argf.Read(buffer) could not fill buffer[:] between files' EOF

--- argf.go
+++ argf.go
@@ -25,16 +25,29 @@ func NewArgf(args []string) (*Argf, error) {
 
 func (this *Argf) Read(data []byte) (int, error) {
 	n, err := this.reader.Read(data)
-	if err == io.EOF {
-		this.reader.Close()
-		this.reader = nil
-		if this.args != nil && len(this.args) >= 1 {
-			this.reader, err = os.Open(this.args[0])
-			if err != nil {
-				return 0, fmt.Errorf("%s: %w", this.args[0], err)
+	for {
+		if err == io.EOF {
+			if this.reader != nil {
+				this.reader.Close()
 			}
-			this.args = this.args[1:]
+			this.reader = nil
+			if this.args != nil && len(this.args) >= 1 {
+				fname := this.args[0]
+				this.args = this.args[1:]
+				this.reader, err = os.Open(fname)
+				if err != nil {
+					return 0, fmt.Errorf("%s: %w", fname, err)
+				}
+			} else {
+				return n, io.EOF
+			}
 		}
+		if n >= len(data) {
+			break
+		}
+		var m int
+		m, err = this.Read(data[n:])
+		n += m
 	}
 	return n, err
 }
