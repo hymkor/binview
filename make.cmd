39996551979c89caa073414ade67b516065e2672
HAYAMA_Kaoru <iyahaya@nifty.com>
2020-11-08 13:25:55 +0900

    make.cmd: implement `make package`

--- make.cmd
+++ make.cmd
@@ -1,9 +1,20 @@
-@setlocal
-@call :"%1"
-@endlocal
-@exit /b
+@echo off
+setlocal
+set "PROMPT=[make] "
+call :"%1"
+endlocal
+exit /b
 
 :""
-    set GOARCH=386
     go build -ldflags "-s -w"
+    exit /b
+
+:"package"
+    for %%I in ("%CD%") do set "NAME=%%~nI"
+    set /P "VERSION=Version ? "
+    for %%I in (386 amd64) do (
+        set GOARCH=%%I
+        call :""
+        zip -9 %NAME%-%VERSION%-%%I.zip %NAME%.exe
+    )
     exit /b
