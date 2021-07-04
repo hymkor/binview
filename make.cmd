f41628a8a081c8ad9a721acb018ff968186f6fed
HAYAMA_Kaoru <iyahaya@nifty.com>
2021-07-05 08:16:06 +0900

    make.cmd: make package: build linux package

--- make.cmd
+++ make.cmd
@@ -19,6 +19,10 @@ exit /b
     for %%I in (386 amd64) do (
         set GOARCH=%%I
         call :""
-        zip -9 %NAME%-%VERSION%-%%I.zip %NAME%.exe
+        zip -9 %NAME%-%VERSION%-windows-%%I.zip %NAME%.exe
     )
+    set GOARCH=amd64
+    set GOOS=linux
+    call :""
+    tar zcvf %NAME%-%VERSION%-linux-amd64.zip %NAME%
     exit /b
