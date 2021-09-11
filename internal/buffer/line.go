0df8ac22427cc076250e30cc18e41ad6abfcf48b
HAYAMA_Kaoru <iyahaya@nifty.com>
2021-09-11 13:29:49 +0900

    Improve allocation for _LINE (16byte to 16KB)

--- internal/buffer/line.go
+++ internal/buffer/line.go
@@ -4,8 +4,17 @@ const LINE_SIZE = 16
 
 type _Line []byte
 
+var allocBuffer []byte
+
 func newLine() _Line {
-	return _Line(make([]byte, LINE_SIZE))
+	// return _Line(make([]byte, LINE_SIZE))
+	if allocBuffer == nil || len(allocBuffer) < LINE_SIZE {
+		allocBuffer = make([]byte, LINE_SIZE*1024)
+	}
+	border := len(allocBuffer) - LINE_SIZE
+	newLine1 := allocBuffer[border:]
+	allocBuffer = allocBuffer[:border]
+	return newLine1
 }
 
 func (line _Line) LastByte() byte {
