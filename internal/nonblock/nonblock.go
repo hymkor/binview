58966dd6da8f66ba63af5bffc6bf82a01ed8a2e7
HAYAMA_Kaoru <iyahaya@nifty.com>
2025-05-15 15:55:11 +0900

    Copy the latest version of  "internal/nonblock" from `hymkor/csvi`

--- internal/nonblock/nonblock.go
+++ internal/nonblock/nonblock.go
@@ -1,48 +1,131 @@
 package nonblock
 
-type _Response struct {
-	data string
-	err  error
+import (
+	"errors"
+	"io"
+	"os"
+	"time"
+)
+
+type keyResponse struct {
+	key string
+	err error
 }
 
-type NonBlock struct {
-	chReq chan struct{}
-	chRes chan _Response
+type dataResponse[T any] struct {
+	val T
+	err error
 }
 
-func New(getter func() (string, error)) *NonBlock {
-	chReq := make(chan struct{})
-	chRes := make(chan _Response)
+type NonBlock[T any] struct {
+	chKeyReq   chan struct{}
+	chKeyRes   chan keyResponse
+	chDataRes  chan dataResponse[T]
+	chStopReq  chan struct{}
+	noMoreData bool
+}
+
+func New[T any](keyGetter func() (string, error),
+	dataGetter func() (T, error)) *NonBlock[T] {
+
+	w := &NonBlock[T]{
+		chKeyReq:  make(chan struct{}),
+		chKeyRes:  make(chan keyResponse),
+		chDataRes: make(chan dataResponse[T]),
+		chStopReq: make(chan struct{}),
+	}
 
 	go func() {
-		for _ = range chReq {
-			data, err := getter()
-			chRes <- _Response{data: data, err: err}
+		for range w.chKeyReq {
+			key, err := keyGetter()
+			w.chKeyRes <- keyResponse{key: key, err: err}
 		}
-		close(chRes)
 	}()
 
-	return &NonBlock{
-		chReq: chReq,
-		chRes: chRes,
-	}
+	go func() {
+		for {
+			select {
+			case <-w.chStopReq:
+				return
+			default:
+				if dataGetter == nil {
+					close(w.chDataRes)
+					return
+				}
+				data, err := dataGetter()
+				w.chDataRes <- dataResponse[T]{val: data, err: err}
+				if errors.Is(err, io.EOF) {
+					close(w.chDataRes)
+					return
+				}
+			}
+		}
+	}()
+
+	return w
 }
 
-func (w *NonBlock) GetOr(work func() bool) (string, error) {
-	w.chReq <- struct{}{}
+func (w *NonBlock[T]) GetOr(work func(val T, err error) bool) (string, error) {
+	w.chKeyReq <- struct{}{}
+	if w.noMoreData {
+		res, ok := <-w.chKeyRes
+		if ok {
+			return res.key, res.err
+		}
+		return "", io.EOF
+	}
 	for {
 		select {
-		case res := <-w.chRes:
-			return res.data, res.err
-		default:
-			if cont := work(); !cont {
-				res := <-w.chRes
-				return res.data, res.err
+		case res, ok := <-w.chKeyRes:
+			if ok {
+				return res.key, res.err
+			}
+		case res, ok := <-w.chDataRes:
+			if !ok || work == nil || !work(res.val, res.err) {
+				res := <-w.chKeyRes
+				w.noMoreData = true
+				return res.key, res.err
 			}
 		}
 	}
 }
 
-func (w *NonBlock) Close() {
-	close(w.chReq)
+func (w *NonBlock[T]) Fetch() (T, error) {
+	res, ok := <-w.chDataRes
+	if !ok {
+		w.noMoreData = true
+		var zero T
+		return zero, io.EOF
+	}
+	if errors.Is(res.err, io.EOF) {
+		w.noMoreData = true
+	}
+	return res.val, res.err
+}
+
+// TryFetch reads a single data item with a timeout.
+// This method is intended for use cases where only data retrieval is needed
+// and no key input is involved.
+// If the timeout expires, it returns os.ErrDeadlineExceeded.
+// If the data input channel is closed, it returns io.EOF.
+func (w *NonBlock[T]) TryFetch(timeout time.Duration) (T, error) {
+	var zero T
+	select {
+	case res, ok := <-w.chDataRes:
+		if ok {
+			if errors.Is(res.err, io.EOF) {
+				w.noMoreData = true
+			}
+			return res.val, res.err
+		}
+		w.noMoreData = true
+		return zero, io.EOF
+	case <-time.After(timeout):
+		return zero, os.ErrDeadlineExceeded
+	}
+}
+
+func (w *NonBlock[T]) Close() {
+	close(w.chStopReq)
+	close(w.chKeyReq)
 }
